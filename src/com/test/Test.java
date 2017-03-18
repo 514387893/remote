08ea1a64b80950e5ae48b9ec1dd4879710cd4079
yanqingyun3 <yanqingyun@jd.com>
2017-03-18 15:01:19 +0800

    my first commit

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -4,7 +4,7 @@ package com.test;
  * Created by yanqingyun on 2017/3/15.
  */
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) {;
         System.out.println("git使用119999");
         System.out.println("local更新1111");
     }
