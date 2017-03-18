fece08597d3ec902fcbbd2378f537ecb98f204cf
514387893 <514387893@qq.com>
2017-03-18 14:57:05 +0800

    kkkk
    kkkkkkkkkkk

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -5,8 +5,7 @@ package com.test;
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println("git使用0");
-
+        System.out.println("git使用119999");
         System.out.println("local更新");
     }
 }
