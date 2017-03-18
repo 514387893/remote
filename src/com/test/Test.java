111cf3195eb999b502041bf3ac4df69376b5e417
yanqingyun3 <yanqingyun@jd.com>
2017-03-18 15:59:34 +0800

    本地999

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -7,6 +7,5 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("git使用第二行");
         System.out.println("local更新第二行");
-        System.out.println("local更新第第88888888888行行");
     }
 }
