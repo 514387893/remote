4af21fd6a3ad9760645b0f1d179c7c64c91e278e
514387893 <514387893@qq.com>
2017-03-26 16:13:46 +0800

    777
    77

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -5,7 +5,7 @@ package com.test;
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println("git使用第二行");
+        System.out.println("git使用第二行777777777");
         System.out.println("local更新第二9999999999999999999999行");
     }
 }
